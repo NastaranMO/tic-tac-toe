160a9adef643a9c385da28b4b41bd2447ea70e5e
Nastaran Moghadasi <nastaran.moghadasi@gmail.com>
2022-11-30 21:01:56 +0100

    add events find match

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,46 +5,62 @@ import { io } from 'socket.io-client';
 const socket = io.connect('http://localhost:3001/');
 
 const storeData = (data) => window.localStorage.setItem('tic-tac-toe', JSON.stringify(data));
-const getData = () => JSON.parse(window.localStorage.getItem('tic-tac-toe')) || '';
+const getData = () => JSON.parse(window.localStorage.getItem('tic-tac-toe')) || {};
 
 function App() {
   const [username, setUsername] = useState('');
+  const [isLogin, setIsLogin] = useState(false);
 
-  // eslint-disable-next-line no-unused-vars
-  const [room, setRoom] = useState('');
-
-  const generateRoom = (e) => {
+  const joinGame = (e) => {
     e.preventDefault();
-    storeData(username);
-    socket.emit('join_game', room);
+    storeData({ username });
+    setIsLogin(true);
+    socket.emit('join_game', username);
+  };
+
+  const findMatch = () => {
+    socket.emit('find_match', username);
   };
 
   useEffect(() => {
     const userData = getData();
-    if (userData) setUsername(userData);
-    setRoom(1);
+    const isEmpty = Object.keys(userData).length === 0;
+    if (!isEmpty) {
+      setUsername(userData.username);
+      setIsLogin(true);
+      console.log('emit message');
+      socket.emit('join_game', userData.username);
+    }
   }, []);
 
   return (
     <div className="App">
       <h1>Online Tictactoe</h1>
-      <h2>
-        Hello
-        {' '}
-        {username}
-      </h2>
-      <form onSubmit={generateRoom}>
-        <label htmlFor="username">
-          Enter your name:
-          <input
-            type="text"
-            id="username"
-            placeholder="John..."
-            onChange={(e) => setUsername(e.target.value)}
-          />
-        </label>
-        <button type="submit">Enter</button>
-      </form>
+      {isLogin
+        ? (
+          <>
+            <h2>
+              Hello
+              {' '}
+              {username}
+            </h2>
+            <button type="submit" onClick={findMatch}>Find a match</button>
+          </>
+        )
+        : (
+          <form onSubmit={joinGame}>
+            <label htmlFor="username">
+              Enter your name:
+              <input
+                type="text"
+                id="username"
+                placeholder="John..."
+                onChange={(e) => setUsername(e.target.value)}
+              />
+            </label>
+            <button type="submit">Enter</button>
+          </form>
+        )}
     </div>
   );
 }
--- server/server.js
+++ server/server.js
@@ -2,9 +2,12 @@ import * as dotenv from 'dotenv'
 import express from 'express'
 import http from 'http'
 import { Server } from 'socket.io'
+import { v4 as uuid } from 'uuid'
 
 dotenv.config()
 const port = process.env.PORT
+let players = []
+const rooms = [];
 
 const app = express()
 const server = http.createServer(app)
@@ -20,14 +23,40 @@ io.on('connection', (socket) => {
 
   socket.on('join_game', (data) => {
     socket.join(data)
-    console.log(`User with id ${socket.id} joined to the game with id ${data}`)
+    const player = {
+      id: socket.id,
+      name: data,
+      matched: false
+    }
+    players.push(player)
 
-    // socket.on('send_message', (data) => {
+    console.log(`User ${player.name} with id ${socket.id} joined to the game`)
+  })
+  // socket.on('send_message', (data) => {
 
-    // })
+  // })
+  socket.on('find_match', (data) => {
+    const freePlayer = players.find(player => !player.matched && player.id !== socket.id)
+
+    if (freePlayer) {
+      socket.emit('response_match', freePlayer)
+
+      console.log(socket.id)
+      const player = players.find(p => p.id === socket.id)
+      player.matched = true
+      freePlayer.matched = true
+
+      console.log(`match the player  ${data} ${socket.id} with ${freePlayer?.name}`)
+      console.log('array of players: ', players)
+    } else {
+      socket.emit('response_match', null)
+      console.log('no match')
+    }
   })
 
   socket.on('disconnect', () => {
+    const updatedPlayers = players.filter(player => player.id !== socket.id)
+    players = updatedPlayers
     console.log(`User ${socket.id} is disconnected`)
   })
 })
