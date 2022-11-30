9c5b27b22468d723111d6373c3c184768940a257
Nastaran Moghadasi <nastaran.moghadasi@gmail.com>
2022-11-30 16:20:04 +0100

    add socket io to project

--- server/server.js
+++ server/server.js
@@ -1,8 +1,35 @@
-import express from 'express'
 import * as dotenv from 'dotenv'
-dotenv.config()
+import express from 'express'
+import http from 'http'
+import { Server } from 'socket.io'
 
+dotenv.config()
 const port = process.env.PORT
 
 const app = express()
-app.listen(port, () => console.log(`Listen on port ${port}`))
+const server = http.createServer(app)
+
+const io = new Server(server, {
+  cors: {
+    origin: '*'
+  }
+})
+
+io.on('connection', (socket) => {
+  console.log(`User ${socket.id} connected`)
+
+  socket.on('join_game', (data) => {
+    socket.join(data)
+    console.log(`User with id ${socket.id} joined to the game with id ${data}`)
+
+    // socket.on('send_message', (data) => {
+
+    // })
+  })
+
+  socket.on('disconnect', () => {
+    console.log(`User ${socket.id} is disconnected`)
+  })
+})
+
+server.listen(port, () => console.log(`Listen on port ${port}`))
