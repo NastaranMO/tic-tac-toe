e706b84fc45822721772632b0fb4983dbf72eb34
Nastaran Moghadasi <nastaran.moghadasi@gmail.com>
2022-11-30 19:07:25 +0100

    add localstorage

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,32 +4,46 @@ import { io } from 'socket.io-client';
 
 const socket = io.connect('http://localhost:3001/');
 
+const storeData = (data) => window.localStorage.setItem('tic-tac-toe', JSON.stringify(data));
+const getData = () => JSON.parse(window.localStorage.getItem('tic-tac-toe')) || '';
+
 function App() {
-  const [msg, setMsg] = useState();
+  const [username, setUsername] = useState('');
+
+  // eslint-disable-next-line no-unused-vars
   const [room, setRoom] = useState('');
 
-  const onChangeHAndler = (e) => {
-    setMsg(e.target.value);
-  };
-  const generateRomm = (e) => {
+  const generateRoom = (e) => {
     e.preventDefault();
+    storeData(username);
     socket.emit('join_game', room);
   };
 
   useEffect(() => {
+    const userData = getData();
+    if (userData) setUsername(userData);
     setRoom(1);
   }, []);
 
   return (
     <div className="App">
-      <form onSubmit={generateRomm}>
-        <input
-          type="text"
-          placeholder="write your message here..."
-          onChange={onChangeHAndler}
-        />
-        <span>{msg}</span>
-        <button type="submit">New game</button>
+      <h1>Online Tictactoe</h1>
+      <h2>
+        Hello
+        {' '}
+        {username}
+      </h2>
+      <form onSubmit={generateRoom}>
+        <label htmlFor="username">
+          Enter your name:
+          <input
+            type="text"
+            id="username"
+            placeholder="John..."
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </label>
+        <button type="submit">Enter</button>
       </form>
     </div>
   );
