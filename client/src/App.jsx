2acce6a695a447a2bdbd2b2995527f52d6940208
Nastaran Moghadasi <nastaran.moghadasi@gmail.com>
2022-12-02 10:36:38 +0100

    rename joinGam

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,10 +9,11 @@ const socket = io.connect('http://localhost:3001');
 function App() {
   const [username, setUsername] = useState('');
   const [room, setRoom] = useState('');
-  const [showGame, setShowGame] = useState(false);
   const [players, setPlayers] = useState([]);
 
-  const joinRoom = async () => {
+  const [showGame, setShowGame] = useState(false);
+
+  const joinGameSubmitHandler = async () => {
     if (username !== '' && room !== '') {
       await socket.emit('join_game', { room, username });
       setShowGame(true);
@@ -34,12 +35,10 @@ function App() {
         showGame
           ? <Game1 players={players} socket={socket} username={username} />
           : (
-            <form onSubmit={joinRoom}>
+            <form onSubmit={joinGameSubmitHandler}>
               <input
                 type="text"
-                onChange={(e) => {
-                  setUsername(e.target.value);
-                }}
+                onChange={(e) => setUsername(e.target.value)}
                 placeholder="John..."
               />
               <input
