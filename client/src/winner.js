afc3715371e02c53ea57ab3b7c22ac0f2c85b225
Nastaran Moghadasi <nastaran.moghadasi@gmail.com>
2022-12-02 19:00:00 +0100

    everything works

--- client/src/winner.js
+++ client/src/winner.js
@@ -1,3 +1,27 @@
+// export default (board, setWinner) => {
+//   for (let i = 0; i < 3; i++) {
+//     if (board[i][0] === board[i][1] === board[i][2] === 'X' || board[i][0] === board[i][1] === board[i][2] === 'O') {
+//       setWinner(board[i][0])
+//       return true
+//     }
+
+//     if (board[0][i] === board[1][i] == board[2][i] === 'X' || board[0][i] === board[1][i] == board[2][i] === 'O') {
+//       setWinner(board[0][i])
+//       return true
+//     }
+
+//     if (board[0, 0] === board[1, 1] === board[2, 2] === "X" || board[0, 0] === board[1, 1] === board[2, 2] === "O") {
+//       setWinner(board[0][i])
+//       return true
+//     }
+
+//     if (board[2, 0] === board[1, 1] === board[0, 2] === 'X' || board[2, 0] === board[1, 1] === board[0, 2] === 'O') {
+//       setWinner(board[0][i])
+//       return true
+//     }
+//   }
+// }
+
 export default (board, setWinner) => {
   for (let i = 0; i < 3; i++) {
     if (board[i][0] === board[i][1] && board[i][1] === board[i][2] && board[i][0] === board[i][2]) {
