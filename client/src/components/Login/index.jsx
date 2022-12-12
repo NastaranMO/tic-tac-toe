799b727e5ec51e5a168dd85be0542bb2bf5d376c
Nastaran Moghadasi <nastaran.moghadasi@gmail.com>
2022-12-12 10:42:12 +0100

    remove extra imports

--- client/src/components/Login/index.jsx
+++ client/src/components/Login/index.jsx
@@ -1,7 +1,5 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './index.css'
-import Header from '../Header';
-import { useState } from 'react';
 import { motion } from 'framer-motion'
 import { store } from '../../utils/store'
 
